f800d5435fef63076e460d3f6dc40e82a6f74850
atzin-em <37525882+atzin-em@users.noreply.github.com>
2019-10-15 08:55:42 +1100

    Android Conversion of Koblinkov

---
+++ KoblinkovAndroid/GameActivity.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KoblinkovAndroid
+{
+    [Activity(Label = "GameActivity")]
+    public class GameActivity : Activity
+    {
+        public Timer TimeTimer = new Timer();
+
+        public int Cur_Turns = 0;
+        public DateTime Cur_Time = new DateTime(1,1,1,0,0,0);
+        public List<Tuple<int[], ImageView>> BarCoords = new List<Tuple<int[], ImageView>>() { };
+        public Java.IO.ISerializable Obj_Game_Mode;
+        public GridLayout Obj_Game_Grid;
+        public TextView TimeScore;
+        public TextView TurnScore;
+        public int cur_int;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_game);
+            Obj_Game_Mode = Intent.GetSerializableExtra("Mode");
+            Typeface KremlinTF = Typeface.CreateFromAsset(this.Assets, "kremlin.ttf");
+            //TimeTimer.Interval = 1000;
+            //TimeTimer.Elapsed += TimerEventProcessor();
+
+            // Declaring views from activity
+            Obj_Game_Grid = (GridLayout)FindViewById(Resource.Id.Game_Grid);
+            TimeScore = (TextView)FindViewById(Resource.Id.Time_TxtView);
+            TurnScore = (TextView)FindViewById(Resource.Id.Turns_TxtView);
+
+            // Set window fade in animation
+            Window.SetWindowAnimations(Android.Resource.Animation.FadeIn);
+
+            // Setup view variables 
+            TimeScore.SetTypeface(KremlinTF, TypefaceStyle.Normal);
+            TurnScore.SetTypeface(KremlinTF, TypefaceStyle.Normal);
+
+            CreateGridFromMode(Obj_Game_Mode.ToString());
+            Randomizer(Obj_Game_Mode.ToString());
+
+        }
+
+        //private ElapsedEventHandler TimerEventProcessor()
+        //{
+        //    Cur_Time = Cur_Time.AddSeconds(1);
+        //    TimeScore.Text = Cur_Time.ToString("mm:ss");
+        //    return new ElapsedEventHandler(this;
+        //}
+        public void CreateGridFromMode(string mode)
+        {
+
+            //Bars.SetImageResource(Application.Assets.Open()
+            int[] SelectedSize = new int[] { };
+            Dictionary<string, int[]> GridSizeDictionary = new Dictionary<string, int[]>
+            {
+                { "Easy"  , new int[] { 4, 4 } },
+                { "Normal", new int[] { 6, 4 } },
+                { "Hard"  , new int[] { 8, 4 } }
+            };
+            SelectedSize = GridSizeDictionary[mode];
+
+            ViewGroup.LayoutParams ImgParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.FillParent, 
+                ViewGroup.LayoutParams.FillParent);
+
+
+            
+            cur_int = SelectedSize[0];
+            Obj_Game_Grid.ColumnCount = SelectedSize[1];
+            Obj_Game_Grid.RowCount = SelectedSize[0];
+
+            if (mode == "Easy")
+            {
+                ImgParams.Width = 160;
+                ImgParams.Height = 280;
+            }
+            else if (mode == "Normal")
+            {
+                ImgParams.Width = 160;
+                ImgParams.Height = 180;
+            }
+            else if (mode == "Hard")
+            {
+                ImgParams.Width = 160;
+                ImgParams.Height = 135;
+            }
+
+            for (int i = 0; i < GridSizeDictionary[mode][0]; i++)
+            {
+                for (int x = 0; x < GridSizeDictionary[mode][1]; x++)
+                {
+                    
+                    ImageView BarImg = new ImageView(this);
+
+                    if (mode == "Easy")
+                    {
+                        BarImg.SetPadding(5, 5, 5, 5);
+                    }
+                    else if (mode == "Normal")
+                    {
+                        BarImg.SetPadding(10, 5, 10, 5);
+                    }
+                    else if (mode == "Hard")
+                    {
+                        BarImg.SetPadding(15, 10, 15, 10);
+                    }
+                    BarImg.SetImageResource(Resource.Drawable.bar);
+                    BarImg.Id = int.Parse(x + "99" + i);
+                    BarImg.LayoutParameters = ImgParams;
+                    Obj_Game_Grid.AddView(BarImg);
+                    BarImg.Touch += Obj_BarImg_Touch;
+                    //BarImg.
+                    BarCoords.Add(new Tuple<int[], ImageView>(
+                        new int[] { i, x },
+                        BarImg));
+                }
+            }
+        }
+
+        private void Randomizer(string mode)
+        {
+            if (mode == "Easy")
+            {
+                RandomSelect(20);
+            }
+            else if (mode == "Normal")
+            {
+                RandomSelect(40);
+            }
+            else if (mode == "Hard")
+            {
+                RandomSelect(200);
+            }
+        }
+
+        private void RandomSelect(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                Random rnd = new Random();
+                int r = rnd.Next(BarCoords.Count);
+                var RandBar = BarCoords[r].Item1;
+                //Console.WriteLine("This was the coordinate x:{0} y:{1}", RandBar[0], RandBar[1]);
+                RotateBars(RandBar[0], RandBar[1], false);
+            }
+        }
+
+        private void RotateBars(int x, int y, bool AnimateBars)
+        {
+            Console.WriteLine("RotateBars was called with params x:{0} y:{1}", x, y);
+            for (int i = 0; i < BarCoords.Count; i++)
+            {
+                if (BarCoords[i].Item1[0] == x)
+                {
+                    if (BarCoords[i].Item2.Rotation == 90)
+                    {
+                        if (AnimateBars == true)
+                        {
+                            BarCoords[i].Item2.Animate().Rotation(0);
+                        }
+                        else
+                        {
+                            BarCoords[i].Item2.Rotation = 0;
+                        }
+                        
+                    }
+                    else if (BarCoords[i].Item2.Rotation == 0)
+                    {
+                        if (AnimateBars == true)
+                        {
+                            BarCoords[i].Item2.Animate().Rotation(90);
+                        }
+                        else
+                        {
+                            BarCoords[i].Item2.Rotation = 90;
+                        }
+                    }
+                }
+                if (BarCoords[i].Item1[1] == y)
+                {
+                    if (BarCoords[i].Item2.Rotation == 90)
+                    {
+                        if (AnimateBars == true)
+                        {
+                            BarCoords[i].Item2.Animate().Rotation(0);
+                        }
+                        else
+                        {
+                            BarCoords[i].Item2.Rotation = 0;
+                        }
+                    }
+                    else if (BarCoords[i].Item2.Rotation == 0)
+                    {
+                        if (AnimateBars == true)
+                        {
+                            BarCoords[i].Item2.Animate().Rotation(90);
+                        }
+                        else
+                        {
+                            BarCoords[i].Item2.Rotation = 90;
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool GameWonCheck()
+        {
+            bool BarsCorrect = false;
+            for (int i = 0; i < BarCoords.Count; i++)
+            {
+                if (BarCoords[i].Item2.Rotation == 0)
+                {
+                    BarsCorrect = true;
+                }
+                else if (BarCoords[i].Item2.Rotation == 90)
+                {
+                    BarsCorrect = false;
+                    
+                    
+                }
+            }
+            return BarsCorrect;
+        }
+
+        private void Obj_BarImg_Touch(object sender, View.TouchEventArgs e)
+        {
+            if (e.Event.Action == MotionEventActions.Up)
+            {
+                ImageView TouchedBar = (ImageView)sender;
+
+                for (int i = 0; i < BarCoords.Count; i++)
+                {
+                    if (BarCoords[i].Item2 == sender)
+                    {
+                        Console.WriteLine("Coords of the touched bar x:{0} y:{1}", BarCoords[i].Item1[0], BarCoords[i].Item1[1]);
+                        //Console.WriteLine(TouchedBar.Id);
+                        //Console.WriteLine(BarCoords[i].Item2.Rotation);
+                        RotateBars(BarCoords[i].Item1[0], BarCoords[i].Item1[1], true);
+                        //TouchedBar.Animate().RotationBy(90);
+                    }
+                }
+                //if (GameWonCheck() == true)
+                //{
+                //    Console.WriteLine("You won!");
+                //}
+            }
+        }
+        public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back)
+            {
+                StartActivity(typeof(MainActivity));
+                OverridePendingTransition(Android.Resource.Animation.FadeIn, Android.Resource.Animation.FadeOut);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ KoblinkovAndroid/MainActivity-----.cs
@@ -0,0 +1,181 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Reflection.Emit;
+using Android.Graphics;
+using Android.Content;
+using Android.Content.Res;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using static Android.Views.View;
+using Android.Views;
+using Android.Content.PM;
+
+namespace KoblinkovAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        //public System.Type ObjGameActivity = typeof(GameActivity);
+        Dictionary<string, int[]> ImageDictionary = new Dictionary<string, int[]> {
+            {"start", new int[] {Resource.Drawable.start_unpressed, Resource.Drawable.start_pressed}},
+            {"settings", new int[] {Resource.Drawable.settings_unpressed, Resource.Drawable.settings_pressed}},
+            {"easy", new int[] {Resource.Drawable.easy_unpressed, Resource.Drawable.easy_pressed}},
+            {"normal", new int[] {Resource.Drawable.normal_unpressed, Resource.Drawable.normal_pressed}},
+            {"hard", new int[] {Resource.Drawable.hard_unpressed, Resource.Drawable.hard_pressed}}
+        };
+        public Intent ObjGameActivity;
+        public Typeface RuskyFont;
+        public TextView Obj_TxtView_Title;
+        public ImageView Obj_Button_Start;
+        public ImageView Obj_Button_Settings;
+        public ImageView Obj_Button_Easy;
+        public ImageView Obj_Button_Normal;
+        public ImageView Obj_Button_Hard;
+
+        private string Game_Difficulty;
+        public event System.EventHandler Difficulty_Changed;
+        public string ChangeDifficulty
+        {
+            get { return Game_Difficulty; }
+            set
+            {
+                Game_Difficulty = value;
+                OnDifficulty_Changed();
+            }
+        }
+
+        protected virtual void OnDifficulty_Changed()
+        {
+            if (Difficulty_Changed != null) Difficulty_Changed(this, EventArgs.Empty);
+            
+                
+                if (Game_Difficulty == "Easy")
+                {
+                    Obj_Button_Easy.SetImageResource(ImageDictionary["easy"][1]);
+                    Obj_Button_Normal.SetImageResource(ImageDictionary["normal"][0]);
+                    Obj_Button_Hard.SetImageResource(ImageDictionary["hard"][0]);
+                }
+                else if (Game_Difficulty == "Normal")
+                {
+                    Obj_Button_Easy.SetImageResource(ImageDictionary["easy"][0]);
+                    Obj_Button_Normal.SetImageResource(ImageDictionary["normal"][1]);
+                    Obj_Button_Hard.SetImageResource(ImageDictionary["hard"][0]);
+                }
+                else if (Game_Difficulty == "Hard")
+                {
+                    Obj_Button_Easy.SetImageResource(ImageDictionary["easy"][0]);
+                    Obj_Button_Normal.SetImageResource(ImageDictionary["normal"][0]);
+                    Obj_Button_Hard.SetImageResource(ImageDictionary["hard"][1]);
+                }
+            
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            View decorView = Window.DecorView;
+            var uiOptions = (int)decorView.SystemUiVisibility;
+            var newUiOptions = (int)uiOptions;
+
+            newUiOptions |= (int)SystemUiFlags.LowProfile;
+            newUiOptions |= (int)SystemUiFlags.Fullscreen;
+            newUiOptions |= (int)SystemUiFlags.HideNavigation;
+            newUiOptions |= (int)SystemUiFlags.Immersive;
+
+            decorView.SystemUiVisibility = (StatusBarVisibility)newUiOptions;
+
+            // Set window fade in animation
+            Window.SetWindowAnimations(Android.Resource.Animation.FadeIn);
+
+            SetContentView(Resource.Layout.activity_main);
+
+
+            // Setting other variables
+            ObjGameActivity = new Intent(this, typeof(GameActivity));
+
+            // Declare items from assets folder
+            Typeface KremlinTF = Typeface.CreateFromAsset (this.Assets, "kremlin.ttf");
+
+            // Declaring views from activity
+            Obj_TxtView_Title = (TextView)FindViewById(Resource.Id.TxtView_Title);
+            Obj_Button_Start = (ImageView)FindViewById(Resource.Id.Start_Btn);
+            Obj_Button_Settings = (ImageView)FindViewById(Resource.Id.Settings_Btn);
+            Obj_Button_Easy = (ImageView)FindViewById(Resource.Id.Easy_Btn);
+            Obj_Button_Normal = (ImageView)FindViewById(Resource.Id.Normal_Btn);
+            Obj_Button_Hard = (ImageView)FindViewById(Resource.Id.Hard_Btn);
+
+            // Changing view options
+            Obj_TxtView_Title.SetTypeface(KremlinTF, TypefaceStyle.Normal);
+            Obj_Button_Start.SetImageResource(ImageDictionary["start"][0]);
+            Obj_Button_Settings.SetImageResource(ImageDictionary["settings"][0]);
+            Obj_Button_Easy.SetImageResource(ImageDictionary["easy"][1]);
+            Obj_Button_Normal.SetImageResource(ImageDictionary["normal"][0]);
+            Obj_Button_Hard.SetImageResource(ImageDictionary["hard"][0]);
+
+            // Binding buttons
+            Obj_Button_Start.Touch += Obj_Button_Start_Touch;
+            Obj_Button_Settings.Touch += Obj_Button_Settings_Touch;
+            Obj_Button_Easy.Touch += Obj_Button_Easy_Touch;
+            Obj_Button_Normal.Touch += Obj_Button_Normal_Touch;
+            Obj_Button_Hard.Touch += Obj_Button_Hard_Touch;
+
+            //Obj_Button_Easy.Touch += Obj_Button_Easy_Touch;
+
+            Game_Difficulty = "Easy";
+        }
+        private void Obj_Button_Easy_Touch(object sender, TouchEventArgs e)
+        {
+            ChangeDifficulty = "Easy";
+        }
+        private void Obj_Button_Normal_Touch(object sender, TouchEventArgs e)
+        {
+            ChangeDifficulty = "Normal";
+        }
+        private void Obj_Button_Hard_Touch(object sender, TouchEventArgs e)
+        {
+            ChangeDifficulty = "Hard";
+        }
+
+        private void Obj_Button_Settings_Touch(object sender, TouchEventArgs e)
+        {
+            if (e.Event.Action == MotionEventActions.Up)
+            {
+                Obj_Button_Settings.SetImageResource(ImageDictionary["settings"][0]);
+            }
+            else if (e.Event.Action == MotionEventActions.Move)
+            {
+                Obj_Button_Settings.SetImageResource(ImageDictionary["settings"][1]);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        
+        private void Obj_Button_Start_Touch(object sender, TouchEventArgs e)
+        {
+            if (e.Event.Action == MotionEventActions.Up)
+            {
+                Obj_Button_Start.SetImageResource(ImageDictionary["start"][0]);
+                ObjGameActivity.PutExtra("Mode", Game_Difficulty);
+                StartActivity(ObjGameActivity);
+                OverridePendingTransition(Android.Resource.Animation.FadeIn, Android.Resource.Animation.FadeOut);
+            }
+            else if (e.Event.Action == MotionEventActions.Move)
+            {
+                Obj_Button_Start.SetImageResource(ImageDictionary["start"][1]);
+            }
+
+        }
+    }
+}
